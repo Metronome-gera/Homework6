c58b44b375642615728bb488eed46db696151af1
Роман <roninmtn@gmail.com>
2025-08-08 10:59:20 +0400

    2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Задача №1
+        for (int i = 1; i <= 10; i++ ){
+            System.out.println(i);
+}
+        // Задача №2
+        for (int i = 10; i >= 1; i-- ){
+            System.out.println(i);
         }
+        // Задача №3
+        for (int i = 0; i <= 17;i = i + 2){
+            System.out.println(i);
+        }
+        // Задача №4
+        for (int i = 10; i >= -10; i  = i - 1){
+            System.out.println(i);
+        }
+        // Задача №5
+        for (int i = 1904; i <= 2096; i= i + 4 ){
+            System.out.println(i + "год является високосным");
+        }
+        //Задача №6
     }
 }

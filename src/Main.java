bbf326c8074f8e71af3f41fd6a3dfd6d44b20220
Роман <roninmtn@gmail.com>
2025-08-11 01:51:58 +0400

    2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Задача №1
         for (int i = 1; i <= 10; i++ ){
             System.out.println(i);
-        }
+}
         // Задача №2
         for (int i = 10; i >= 1; i-- ){
             System.out.println(i);
@@ -23,7 +23,13 @@ public class Main {
             System.out.println(i + "год является високосным");
         }
         // Задача №6
+        for (int i = 0; i <= 100; i=i+7 ){
+            System.out.println(i);
+        }
         // Задача №7
+        for (int i = 1; i <= 512; i= i * 2 ){
+            System.out.println(i);
+        }
         // Задача №8
         int mony = 29000;
         int total = 0;
@@ -32,6 +38,20 @@ public class Main {
             System.out.printf("Месяц " +i+ " сумма накопленийравна " +total+ " рублей");
         }
         // Задача №9
+        int salary = 29000;
+        int full = 0;
+        for (int i = 0; i <=12; i++ ){
+            full = full + full/100;
+            full = full + salary;
+            System.out.printf("Месяц " +i+ " сумма накопленийравна " +full+ " рублей");
+        }
         // Задача №10
+        int number =2;
+        int factor = 2;
+        for (int i = 1; i <= 9; i++ ){
+            factor = number*i;
+            System.out.printf(number+" * " + i+" = "+ factor+"; ");
+
+        }
     }
 }
